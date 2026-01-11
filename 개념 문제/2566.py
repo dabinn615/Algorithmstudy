a8bbfa1400bcee7fc2d7ff0ae538b11a01750907
binn <dabinn615@naver.com>
2026-01-11 21:38:44 +0900

    Update 2566.py

--- 개념 문제/2566.py
+++ 개념 문제/2566.py
@@ -1,1 +1,14 @@
-
+lst=[]
+for i in range (9):
+    nums=input("").split(" ")
+    for j in range (9):
+        lst.append(int(nums[j]))
+print(max(lst))
+max=max(lst)
+cnt=0
+for i in lst:
+    if i==max:
+        print(cnt//9+1,cnt%9+1)
+        break
+    else:
+        cnt+=1
