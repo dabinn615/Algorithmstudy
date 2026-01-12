ccd6fd88c18401a3d4ea07d1c3b9d629004779c3
binn <dabinn615@naver.com>
2026-01-12 23:00:38 +0900

    Update 1157.py

--- 개념 문제/1157.py
+++ 개념 문제/1157.py
@@ -1,1 +1,25 @@
+word=input("").upper().split()
+lst=[i for i in word[0]]
 
+setlst=set(lst)
+setlst=list(setlst)
+
+cnt=0
+cntlst=[]
+dic={}
+for i in setlst:
+    dic.update({i:lst.count(i)})
+
+value=[]
+for v in dic.values():
+    value.append(v)
+
+maxx=max(value)
+if value.count(maxx)!=1:
+    print("?")
+else:
+    for k,v in dic.items():
+        if v==maxx:
+            print(k)
+        else:
+            pass
