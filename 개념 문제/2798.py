8628bc4d15f22f1b298cb37f86cfaf857e01d36d
binn <dabinn615@naver.com>
2026-01-13 00:46:29 +0900

    Update 2798.py

--- 개념 문제/2798.py
+++ 개념 문제/2798.py
@@ -1,1 +1,33 @@
+nm=input("").split(" ")
+n=int(nm[0]); m=int(nm[1])
+nums=input("").split(" ")
+lst=[int(i) for i in nums]
 
+sum=0
+sumlst=[]
+for i in lst:
+    for j in lst:
+        for k in lst:
+            if i!=j and j!=k and k!=i:
+                sum=(i+j+k)
+                sumlst.append(sum)
+            else:
+                pass
+sumlst=set(sumlst)                    
+
+total=[]
+cnt=0
+for i in sumlst:
+    if i==m:
+        print(i)
+        cnt+=1
+    else: 
+        if i<m:
+            total.append(i)
+        else:
+            pass
+if cnt==1:
+    pass
+else:
+    total.sort()
+    print(total[-1]) 
