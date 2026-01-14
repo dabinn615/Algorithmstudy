d3f66a362ae2b9ef2cc9874cc401fe8fc7affe91
binn <dabinn615@naver.com>
2026-01-14 11:33:38 +0900

    Update 2525.py

--- 개념 문제/2525.py
+++ 개념 문제/2525.py
@@ -1,1 +1,14 @@
+ab=input("").split()
+a=int(ab[0]); b=int(ab[1])
+c=int(input(""))
 
+hour=c//60; min=c%60
+a+=hour; b+=min
+
+if b>=60:
+    a+=(b//60)
+    b%=60
+if a>=24:
+    a%=24
+
+print(a,b)
