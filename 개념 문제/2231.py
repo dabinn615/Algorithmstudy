2761979213e692029025b2e9fc5d5f6f917e1853
binn <dabinn615@naver.com>
2026-01-14 11:18:31 +0900

    Update 2231.py

--- 개념 문제/2231.py
+++ 개념 문제/2231.py
@@ -1,1 +1,17 @@
+num=input("")
+lenn=len(num)
+num=int(num)
+lst=[]
+total=[]
 
+for i in range (1,num+1):
+    i=str(i)
+    lst=[int(j) for j in i]
+    i=int(i)
+    if i+sum(lst)==num:
+        total.append(i)
+
+if len(total)!=0:
+    print(total[0])
+else:
+    print("0")
