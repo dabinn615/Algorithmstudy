86502ce5dfe1a200e69338544724a3b67f8afc96
binn <dabinn615@naver.com>
2026-01-15 21:54:52 +0900

    Update 같은 숫자는 싫어 >~<.py

--- 스택,큐/같은 숫자는 싫어 >~<.py
+++ 스택,큐/같은 숫자는 싫어 >~<.py
@@ -1,1 +1,15 @@
+def solution(arr):
+    answer = []
+    for i in range (0,len(arr)):
+        if i==len(arr)-1:
+            answer.append(arr[i])
+            
+        else:
+            if arr[i]==arr[i+1]:
+                pass
+            else:
+                answer.append(arr[i])
+    return answer
 
+arr=[int(i) for i in input("").split(",")]
+print(solution(arr))
