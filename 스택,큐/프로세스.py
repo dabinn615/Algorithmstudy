0468a05c7677b5c3ca917c68d45e0181cfa1e3e6
binn <dabinn615@naver.com>
2026-01-20 22:13:31 +0900

    Add solution function for task prioritization
    Implement a solution to process priorities and determine the order of tasks based on their priority.

--- 스택,큐/프로세스.py
+++ 스택,큐/프로세스.py
@@ -1,1 +1,33 @@
+def solution(priorities, location):
+    answer = 0
+    lst=[]
+    cnt=0
 
+    for i in priorities:
+        lst.append((i,cnt))
+        cnt+=1
+
+    total=[]
+    while 1:
+        maxnum=max(priorities)
+        for i in range(len(lst)):
+            if lst[i][0]==maxnum:
+                total.append(lst[i])
+                priorities.remove(lst[i][0])
+                lst=lst[(i+1):]+lst[:i]
+                break
+                
+        if len(priorities)==0:
+            break
+
+    for i in total:
+        if i[1]==location:
+            answer=(answer+1)
+            break
+        else:
+            answer+=1
+    return answer
+
+priorities=[int(i) for i in input("").split(",")]
+location=int(input("")) #배열의 자리 위치
+print(solution(priorities, location))
